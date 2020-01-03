b18465a0b9edfdaffb95545587f1ece0e52dcb9b
Gerson Alejandro Beltetón Urbina <gersonbelur@gmail.com>
2020-01-03 03:27:35 -0600

    [UPDATED] TopPuntosJuego.java, Register.java, ListUsers.java, Game.form

--- src/org/grupo11/gui/Register.java
+++ src/org/grupo11/gui/Register.java
@@ -34,8 +34,6 @@ public class Register extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel5 = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -45,130 +43,86 @@ public class Register extends javax.swing.JFrame {
         jTextField2 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/grupo11/recursos/Dashboard.png"))); // NOI18N
-        jLabel5.setText("jLabel5");
-
-        jPanel1.setBackground(new java.awt.Color(240, 104, 109));
+        setPreferredSize(new java.awt.Dimension(1280, 740));
+        setSize(new java.awt.Dimension(1280, 740));
+        getContentPane().setLayout(null);
 
         jLabel2.setBackground(new java.awt.Color(255, 255, 153));
-        jLabel2.setFont(new java.awt.Font("Segoe UI Light", 0, 70)); // NOI18N
-        jLabel2.setText("REGISTRO DE USUARIO");
+        jLabel2.setFont(new java.awt.Font("Segoe UI Light", 0, 130)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Registro");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(60, 50, 720, 180);
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI Light", 0, 20)); // NOI18N
-        jLabel1.setText("USUARIO");
+        jLabel1.setFont(new java.awt.Font("Segoe UI Light", 0, 30)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Usuario");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(80, 240, 160, 41);
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI Light", 0, 20)); // NOI18N
-        jLabel3.setText("CONTRASEÑA");
+        jLabel3.setFont(new java.awt.Font("Segoe UI Light", 0, 30)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Contraseña");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(80, 330, 200, 41);
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI Light", 0, 20)); // NOI18N
-        jLabel4.setText("CONFIRMAR CONTRASEÑA");
+        jLabel4.setFont(new java.awt.Font("Segoe UI Light", 0, 30)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Confirmar Contraseña");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(80, 420, 310, 41);
 
         jTextField3.setFont(new java.awt.Font("Segoe UI Light", 0, 20)); // NOI18N
+        getContentPane().add(jTextField3);
+        jTextField3.setBounds(80, 470, 350, 33);
 
         jTextField1.setFont(new java.awt.Font("Segoe UI Light", 0, 20)); // NOI18N
+        getContentPane().add(jTextField1);
+        jTextField1.setBounds(80, 290, 350, 33);
 
         jTextField2.setFont(new java.awt.Font("Segoe UI Light", 0, 20)); // NOI18N
+        getContentPane().add(jTextField2);
+        jTextField2.setBounds(80, 380, 350, 33);
 
-        jButton1.setFont(new java.awt.Font("Segoe UI Light", 0, 20)); // NOI18N
-        jButton1.setText("ENVIAR SOLICITUD");
+        jButton1.setFont(new java.awt.Font("Segoe UI Light", 0, 24)); // NOI18N
+        jButton1.setText("Enviar Solicitud");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(80, 540, 250, 41);
 
-        jButton2.setFont(new java.awt.Font("Segoe UI Light", 0, 20)); // NOI18N
-        jButton2.setText("REGRESAR");
+        jButton2.setFont(new java.awt.Font("Segoe UI Light", 0, 24)); // NOI18N
+        jButton2.setText("<< Regresar");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton2);
+        jButton2.setBounds(370, 540, 200, 41);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 769, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addGap(0, 0, Short.MAX_VALUE))))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(223, 223, 223)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(95, 95, 95)
-                        .addComponent(jButton1)
-                        .addGap(56, 56, 56)
-                        .addComponent(jButton2)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(33, 33, 33)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(41, 41, 41)
-                .addComponent(jLabel1)
-                .addGap(3, 3, 3)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(37, 37, 37)
-                .addComponent(jLabel3)
-                .addGap(29, 29, 29)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel4)
-                .addGap(18, 18, 18)
-                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(60, 60, 60)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 401, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 717, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 681, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/grupo11/recursos/Registro.png"))); // NOI18N
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(-30, 0, 1280, 720);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        Login lg = new Login();
+        lg.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         boolean existing = false;
-        
+
         for (User user: LoggedUser.solicitud) {
             if (user.getUsername().equals(this.jTextField1.getText())) {
                 JOptionPane.showMessageDialog(null, "Este usuario ya existe en el sistema");
@@ -176,22 +130,15 @@ public class Register extends javax.swing.JFrame {
             }
         }
         if (existing == false) {
-        if (this.jTextField2.getText().equals(this.jTextField3.getText())) {
-           JOptionPane.showMessageDialog(null, "Esperando aprobación");
-           LoggedUser.solicitud.add(new User(this.jTextField1.getText(),this.jTextField2.getText()));
-            System.out.println(LoggedUser.solicitud.size());
-        }else {
-            JOptionPane.showMessageDialog(null, "Las contraseñas no coinciden");
+            if (this.jTextField2.getText().equals(this.jTextField3.getText())) {
+                JOptionPane.showMessageDialog(null, "Esperando aprobación");
+                LoggedUser.solicitud.add(new User(this.jTextField1.getText(),this.jTextField2.getText()));
+                System.out.println(LoggedUser.solicitud.size());
+            }else {
+                JOptionPane.showMessageDialog(null, "Las contraseñas no coinciden");
+            }
         }
-        }
-
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        Login lg = new Login();
-        lg.setVisible(true);
-        this.setVisible(false);
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -235,8 +182,7 @@ public class Register extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
